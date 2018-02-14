b34a17e7184b861679f1fbd29b8a0b195cf5aa98
Imran Qadeer <imran.qadeer@saga.co.uk>
2018-02-14 16:47:43 +0000

    Added initial files.

---
+++ BOA.WeatherForcast/BOA.WeatherForcast.Web/Controllers/AppController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace BOA.WeatherForcast.Web.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            //throw new InvalidOperationException();
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
