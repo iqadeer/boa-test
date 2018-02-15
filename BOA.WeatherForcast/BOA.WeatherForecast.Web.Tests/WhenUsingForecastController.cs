98dd2c378d65127255f730d9d567dc6320a6dac7
Imran Qadeer <imran.qadeer@saga.co.uk>
2018-02-15 19:09:22 +0000

    Added first api unit test

---
+++ BOA.WeatherForcast/BOA.WeatherForecast.Web.Tests/WhenUsingForecastController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using BOA.WeatherForcast.Web.Controllers;
+using BOA.WeatherForcast.Web.Services;
+using BOA.WeatherForcast.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+
+namespace BOA.WeatherForecast.Web.Tests
+{
+    [TestFixture]
+    public class WhenUsingForecastController
+    {
+        private ForecastController _forecastController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var foreCastService = new Mock<IForecastService>();
+
+            foreCastService.Setup(m => m.GetCities(It.IsAny<string>()))
+                .Returns(new List<CityViewModel>());
+
+            var logger = new Mock<ILogger<ForecastController>>();
+
+            _forecastController = new ForecastController(logger.Object, foreCastService.Object);
+        }
+
+        [Test]
+        public void when_calling_GetCities_should_return_all_cities()
+        {
+            var result = _forecastController.Get();
+
+            Assert.NotNull(result);
+
+            var objectResult = result as OkObjectResult;
+
+            Assert.NotNull(objectResult);
+
+            Assert.NotNull(objectResult.Value);
+            Assert.IsInstanceOf<IEnumerable<CityViewModel>>(objectResult.Value);
+        }
+        
+    }
+}
