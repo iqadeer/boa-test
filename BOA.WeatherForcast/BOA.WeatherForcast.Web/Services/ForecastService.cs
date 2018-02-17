932285bd35bddac12eb21a9dd0c6878226930684
Imran Qadeer <imran.qadeer@saga.co.uk>
2018-02-17 11:37:12 +0000

    Added configuration

--- BOA.WeatherForcast/BOA.WeatherForcast.Web/Services/ForecastService.cs
+++ BOA.WeatherForcast/BOA.WeatherForcast.Web/Services/ForecastService.cs
@@ -6,6 +6,7 @@ using AutoMapper;
 using BOA.WeatherForcast.Web.ViewModels;
 using BOA.WeatherForecast.Data;
 using BOA.WeatherForecast.Data.Entities;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
 namespace BOA.WeatherForcast.Web.Services
@@ -15,12 +16,14 @@ namespace BOA.WeatherForcast.Web.Services
         private readonly IRepository<City> _repository;
         private readonly IMapper _mapper;
         private readonly IHttpClientWrapper _client;
+        private readonly IConfiguration _configuration;
 
-        public ForecastService(IRepository<City> repository, IMapper mapper, IHttpClientWrapper client)
+        public ForecastService(IRepository<City> repository, IMapper mapper, IHttpClientWrapper client, IConfiguration configuration)
         {
             _repository = repository;
             _mapper = mapper;
             _client = client;
+            _configuration = configuration;
         }
 
         public IEnumerable<CityViewModel> GetCities(string country)
@@ -30,12 +33,16 @@ namespace BOA.WeatherForcast.Web.Services
 
         public async Task<WeatherForecast.Domain.WeatherForecast> GetWeatherForecast(int id)
         {
-                var path =
-                    $"http://api.openweathermap.org/data/2.5/forecast?id={id}&units=metric&appid=d958826a9d8f775f24dbeddcf2838369";
+            //$"{_configuration["WeatherServiceBaseUrl"]}?id={id}&units={}"
+            var path = $"http://api.openweathermap.org/data/2.5/forecast?id={id}&units=metric&appid=d958826a9d8f775f24dbeddcf2838369";
                 var response = await _client.GetAsync(path);
                 response.EnsureSuccessStatusCode();
                 var contentString = await response.Content.ReadAsStringAsync();
                 return JsonConvert.DeserializeObject<WeatherForecast.Domain.WeatherForecast>(contentString);
+
+//            "WeatherServiceBaseUrl": "http://api.openweathermap.org/data/2.5/forecast",
+  //          "AppId": "d958826a9d8f775f24dbeddcf2838369"
+
         }
 
         public void Dispose()
--- BOA.WeatherForcast/BOA.WeatherForecast.Web.Tests/WhenUsingForecastController.cs
+++ BOA.WeatherForcast/BOA.WeatherForecast.Web.Tests/WhenUsingForecastController.cs
@@ -28,19 +28,30 @@ namespace BOA.WeatherForecast.Web.Tests
         }
 
         [Test]
-        public void when_calling_GetCities_should_return_all_cities()
+        public void when_calling_GetCities_should_return_list_of_cites_if_serivce_return_ok()
+        {
+            var result = _forecastController.Get();
+            var objectResult = result as OkObjectResult;
+            Assert.IsInstanceOf<IEnumerable<CityViewModel>>(objectResult.Value);
+        }
+        [Test]
+        public void when_calling_GetCities_should_not_return_null()
         {
             var result = _forecastController.Get();
 
             Assert.NotNull(result);
 
+        }
+        [Test]
+        public void when_calling_GetCities_should_return_OkObjectResult_if_proxy_returns_data()
+        {
+            var result = _forecastController.Get();
+
             var objectResult = result as OkObjectResult;
 
             Assert.NotNull(objectResult);
 
-            Assert.NotNull(objectResult.Value);
-            Assert.IsInstanceOf<IEnumerable<CityViewModel>>(objectResult.Value);
         }
-        
+
     }
 }
