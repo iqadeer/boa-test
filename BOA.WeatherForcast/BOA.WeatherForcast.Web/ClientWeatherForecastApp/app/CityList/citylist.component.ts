92ba46c3193a47597e0ab0acd47e64fa118f9a82
Imran Qadeer <imran.qadeer@saga.co.uk>
2018-02-18 02:54:03 +0000

    Added citylist component.

---
+++ BOA.WeatherForcast/BOA.WeatherForcast.Web/ClientWeatherForecastApp/app/CityList/citylist.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'cities-list',
+    templateUrl: "./citylist.component.html",
+})
+export class CityListComponent {
+    title = "Five day weather forecast";
+}
--- BOA.WeatherForcast/BOA.WeatherForcast.Web/ClientWeatherForecastApp/app/app.component.ts
+++ BOA.WeatherForcast/BOA.WeatherForcast.Web/ClientWeatherForecastApp/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'forecast-app',
   templateUrl: "./app.component.html",
-  styles: []
 })
 export class AppComponent {
   title = "Five day weather forecast";
